d7e22ac6751bb33191bb8248353a1cc6a698692d
Kevin Chiu <kevin@kevinchiu.org>
2009-10-11 23:43:31 -0400

    enabling image emails

--- app/controllers/email_controller.rb
+++ app/controllers/email_controller.rb
@@ -4,6 +4,12 @@ class EmailController < ApplicationController
     subject = params[:subject]
     body = params[:body]
     
+    if(params[:data])
+      attachment = params[:data]
+      attachment_type = "image/jpeg"
+      Notifier.deliver_email(to, subject, body, attachment, attachment_type)
+    end
+    
     Notifier.deliver_email(to, subject, body)
   end
 end
--- app/models/notifier.rb
+++ app/models/notifier.rb
@@ -1,10 +1,15 @@
 class Notifier < ActionMailer::Base
   
-  def email(email, subject, message)
+  def email(email, subject, message, attachment, attachment_type)
     recipients  email
     from        "noreply@visionontap.com"
     subject     subject
     body        message
+    if(attachment)
+      attachment  :content_type => attachment_type,
+                  :body => attachment
+    end
+    
   end
 
 end
