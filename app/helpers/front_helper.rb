71e78e6e41e1563419da15429f59cea73659b436
Kevin Chiu <kevin@kevinchiu.org>
2009-10-15 19:30:20 -0400

    added video

--- app/helpers/front_helper.rb
+++ app/helpers/front_helper.rb
@@ -16,4 +16,10 @@ module FrontHelper
     eos
   end
   
+  def vimeo
+    <<-eos
+      <object width="500" height="281"><param name="allowfullscreen" value="true" /><param name="allowscriptaccess" value="always" /><param name="movie" value="http://vimeo.com/moogaloop.swf?clip_id=7089181&amp;server=vimeo.com&amp;show_title=1&amp;show_byline=1&amp;show_portrait=0&amp;color=00ADEF&amp;fullscreen=1" /><embed src="http://vimeo.com/moogaloop.swf?clip_id=7089181&amp;server=vimeo.com&amp;show_title=1&amp;show_byline=1&amp;show_portrait=0&amp;color=00ADEF&amp;fullscreen=1" type="application/x-shockwave-flash" allowfullscreen="true" allowscriptaccess="always" width="500" height="281"></embed></object></p>
+    eos
+  end
+  
 end
